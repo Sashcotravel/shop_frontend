6424e10e5d3335e3fe08e4416c291b77fec7556e
Sashcotravel <vasinoleksandr1@gmail.com>
2023-01-15 16:26:21 +0200

    12.5

--- shop/src/Header/index.jsx
+++ shop/src/Header/index.jsx
@@ -10,27 +10,34 @@ export const Header = ({ t, changeLanguage, lang }) => {
 
   const click = () => {
     const id = document.getElementsByClassName("menu__btn");
-    id[0].style.visibility = "hidden";
+    // id[0].style.visibility ="hidden"
+    // if(id[0].style.visibility === "visible"){
+    //   id[0].style.visibility = "hidden"
+    // } else {
+    //   id[0].style.visibility = "visible"
+    // }
     const id2 = document.getElementsByClassName("menu__box");
-    id2[0].style.visibility = "visible";
-    console.log(id, id2);
+    // // id2[0].style.visibility = "visible";
+    if(id2[0].style.visibility === "visible"){
+      id2[0].style.visibility = "hidden"
+    } else {
+      id2[0].style.visibility = "visible"
+    }
   };
 
-  const click2 = () => {
-    // const id = document.getElementsByClassName("menu__btn");
-    // id[0].style.visibility = "visible";
-    // const id2 = document.getElementsByClassName("menu__box");
-    // id2[0].style.visibility = "hidden";
-
-    // console.log(id, id2);
-  };
-
+  // const click2 = () => {
+  //   const id = document.getElementsByClassName("menu__btn");
+  //   id[0].style.visibility = "visible";
+  //   const id2 = document.getElementsByClassName("menu__box");
+  //   id2[0].style.visibility = "hidden";
+  //   console.log('lol2');
+  // };
 
   return (
     <header>
       <div className="root">
-        <Container maxWidth="lg">
-          <div className="inner">
+        {/*<Container maxWidth="lg">*/}
+        {/*  <div className="inner">*/}
             <div>
               <Link to="/">
                 <img className="logo" src={image5} />
@@ -51,25 +58,42 @@ export const Header = ({ t, changeLanguage, lang }) => {
                 </button>
               </div>
             </div>
-            <div className="hamburger-menu" style={{ zIndex: "1" }}>
-              <input id="menu__toggle" type="checkbox" />
-              <label className="menu__btn" htmlFor="menu__toggle">
-                <span></span>
-              </label>
-              <span onClick={click2}>
-            <ul className="menu__box">
-              <li><NavLink className="menu__item" to="/">{t("mainPage")}</NavLink></li>
-              <li><NavLink className="menu__item"
+            {
+              window.screen.width > 900
+                ?
+                <div className='manu'>
+                  {/*<div className="menu__item">*/}
+                    <Link className="menu__item"  to="/">{t("mainPage")}</Link>
+                  {/*</div>*/}
+                  {/*<div className="menu__item">*/}
+                    <Link className="menu__item" to="/obl">{t("aboutUs")}</Link>
+                  {/*</div>*/}
+                  <div className="menu__item">Про нас</div>
+                  <div className="menu__item">{t("blog")}</div>
+                  <div className="menu__item">{t("contacts")}</div>
+                </div>
+                : <>
+                  <div className="hamburger-menu" style={{ zIndex: "1" }}>
+                    <input id="menu__toggle" type="checkbox" onClick={click} />
+                    <label className="menu__btn" htmlFor="menu__toggle">
+                      <span></span>
+                    </label>
+                    <span>
+                      <ul className="menu__box">
+                         <li><NavLink className="menu__item" to="/">{t("mainPage")}</NavLink></li>
+                          <li><NavLink className="menu__item"
                            to="/obl">{t("aboutUs")}</NavLink></li>
-              <li><a className="menu__item" href="#">{t("team")}</a></li>
-              <li><a className="menu__item" href="#">{t("blog")}</a></li>
-              <li><a className="menu__item" href="#">{t("contacts")}</a></li>
-            </ul>
-            </span>
-            </div>
+                        <li><a className="menu__item" href="#">{t("team")}</a></li>
+                        <li><a className="menu__item" href="#">{t("blog")}</a></li>
+                        <li><a className="menu__item" style={{borderBottom: '1px solid whitesmoke'}} href="#">{t("contacts")}</a></li>
+                      </ul>
+                    </span>
+                  </div>
+                </>
+            }
           </div>
-        </Container>
-      </div>
+        {/*</Container>*/}
+      {/*</div>*/}
     </header>
   );
 };
